1d0810191d3a59dadba75d77834dc435699d5685
Ryan <chen3194@purdue.edu>
2023-01-21 14:01:33 -0500

    constructor

--- env.py
+++ env.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import pygame
 
 class Reversi:
     EMPTY = 0
@@ -7,15 +7,28 @@ class Reversi:
     BLACK = 2
 
     def __init__(self, size):
-        action_space = []  # the possible actions the next player / agent can make
-        grid = [[Reversi.EMPTY for _ in range(size)] for _ in range(size)]
+        assert(size % 2 == 0 and size > 2)
+        self.size = size
+        self.grid = [[Reversi.EMPTY for _ in range(size)] for _ in range(size)] # [row][column]
+        middle = int(self.size / 2)
+        self.grid[middle][middle] = Reversi.BLACK
+        self.grid[middle - 1][middle - 1] = Reversi.BLACK
+        self.grid[middle][middle- 1] = Reversi.WHITE
+        self.grid[middle- 1][middle] = Reversi.WHITE
+        action_space = [(middle - 2, middle),(middle -1, middle + 1),(middle, middle -2),(middle +1, middle -1)] # the possible actions the next player / agent can make
 
-    # update the grid and action space
+    # update the grid and action space after the player make an action
     def step(self, action):
-        return next_state, reward, done
+        pass
+        #return next_state, reward, done
 
     def reset(self):
         pass
 
     def rendor(self):
-        pass
+        for row in self.grid:
+            print(row)
+
+if __name__=="__main__":
+    r = Reversi(8)
+    r.rendor()
